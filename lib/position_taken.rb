7087278ad07d2c9433b03a0cad4eb80aba63a25f
Lenn Hypolite <lenntriniman@hotmail.com>
2016-03-31 20:41:09 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,7 @@
 # code your #position_taken? method here!
+def position_taken?(board, position)
+     board[position] == "" || board[position] == " " || board[position] == nil
+      return false
+     board[position] == "X" || board[position] == "O"
+      return true
+end
