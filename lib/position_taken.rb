770ee6655869cb591ece8aa83593173154fc5c50
Lenn Hypolite <lenntriniman@hotmail.com>
2016-03-31 20:44:28 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,6 +2,6 @@
 def position_taken?(board, position)
      board[position] == "" || board[position] == " " || board[position] == nil
       return false
-     board[position] == "X" || board[position] == "O"
+     board[position =0] == "X" || board[position=0] == "O"
       return true
 end
